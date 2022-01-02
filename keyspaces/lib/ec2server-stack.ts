084b77ce0a74264998cbaf2356543b20c3b472d5
matthias <matthias.fuchs>
2022-01-02 17:09:06 +0100

    add queues

--- keyspaces/lib/ec2server-stack.ts
+++ keyspaces/lib/ec2server-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as autoscaling from '@aws-cdk/aws-autoscaling'
+import { Http2ServerRequest } from 'http2';
 
 
 export class Ec2serverStack extends cdk.Stack {
@@ -7,12 +10,19 @@ export class Ec2serverStack extends cdk.Stack {
     super(scope, id, props);
 
     // 👇 create VPC in which we'll launch the Instance
+
     const vpc = new ec2.Vpc(this, 'colorado-vpc', {
       cidr: '10.0.0.0/16',
       natGateways: 0,
       subnetConfiguration: [
         { name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC },
       ],
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
+      vpc,
+      internetFacing: true,
+      idleTimeout: cdk.Duration.seconds(2000),
     });
 
     // 👇 create Security Group for the Instance
@@ -28,6 +38,7 @@ export class Ec2serverStack extends cdk.Stack {
     );
 
     // 👇 create the EC2 Instance
+    /*
     const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
       vpc,
       vpcSubnets: {
@@ -35,15 +46,104 @@ export class Ec2serverStack extends cdk.Stack {
       },
       securityGroup: webserverSG,
       instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T3,
+        ec2.InstanceClass.T4G,
         ec2.InstanceSize.MICRO,
       ),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      //machineImage: new ec2.AmazonLinuxImage({
+      //  generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      //}),
+      machineImage: new ec2.GenericLinuxImage({
+        'eu-central-1': 'ami-0b168c89474ef4301'
       }),
       keyName: 'ec2-key-pair',
     });
+    */
 
+
+    const baseasg = new autoscaling.AutoScalingGroup(this, 'baseasg', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.MICRO,
+      ),
+      //machineImage: new ec2.AmazonLinuxImage({
+      //  generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      //}),
+      machineImage: new ec2.GenericLinuxImage({
+        'eu-central-1': 'ami-0b168c89474ef4301'
+      }),
+      keyName: 'ec2-key-pair',
+      minCapacity: 1,
+      maxCapacity: 1,
+    });
+
+    const wsasg = new autoscaling.AutoScalingGroup(this, 'wsasg', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.MICRO,
+      ),
+      //machineImage: new ec2.AmazonLinuxImage({
+      //  generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      //}),
+      machineImage: new ec2.GenericLinuxImage({
+        'eu-central-1': 'ami-0b168c89474ef4301'
+      }),
+      keyName: 'ec2-key-pair',
+      minCapacity: 1,
+      maxCapacity: 1,
+    });
+
+    const listener = alb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+
+    listener.addTargets('default-target', {
+      port: 8080,
+      targets: [baseasg],
+      healthCheck: {
+        path: '/',
+        unhealthyThresholdCount: 2,
+        healthyThresholdCount: 3,
+        interval: cdk.Duration.seconds(30),
+      },
+    });
+
+    const wstarget = listener.addTargets('wstarget', {
+      port: 4001,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [wsasg],
+      healthCheck: {
+        path: '/',
+        unhealthyThresholdCount: 2,
+        healthyThresholdCount: 3,
+        interval: cdk.Duration.seconds(30),
+      },
+    });
+
+    new elbv2.ApplicationListenerRule(this, 'wsPathListenerRule', {
+      listener: listener,
+      priority: 10,
+      conditions: [
+        elbv2.ListenerCondition.pathPatterns(['/ws'])
+      ],
+      action: elbv2.ListenerAction.forward([wstarget])
+    })
+
+    new cdk.CfnOutput(this, 'albDNS', {
+      value: alb.loadBalancerDnsName,
+    });
+
+    // Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - ami-0b168c89474ef4301
 
   }
 }
--- keyspaces/lib/keyspaces-stack.ts
+++ keyspaces/lib/keyspaces-stack.ts
@@ -1,20 +1,13 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as iam from '@aws-cdk/aws-iam';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as iot from '@aws-cdk/aws-iot';
 import * as cassandra from '@aws-cdk/aws-cassandra';
-
-import * as tableColumns from  './tableColumns';
+import * as tableColumns from './tableColumns';
 
 export class KeyspacesStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'KeyspacesQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'KeyspacesTopic');
 
     var keyspaceName = 'colorado';
     var tableNameHeatdata = 'heatdata';
@@ -24,7 +17,7 @@ export class KeyspacesStack extends cdk.Stack {
       keyspaceName: keyspaceName
     });
 
-    new cassandra.CfnTable(this,tableNameHeatdata,{
+    new cassandra.CfnTable(this, tableNameHeatdata, {
       tableName: tableNameHeatdata,
       keyspaceName: keyspaceName,
       partitionKeyColumns: [{
@@ -36,26 +29,90 @@ export class KeyspacesStack extends cdk.Stack {
     }).addDependsOn(keyspace)
 
     // ordering missing
-    new cassandra.CfnTable(this,tableNameHeatids,{
+    new cassandra.CfnTable(this, tableNameHeatids, {
       tableName: tableNameHeatids,
       keyspaceName: keyspaceName,
       partitionKeyColumns: [{
         columnName: 'wkid',
         columnType: 'int',
       }
-    ],
-    clusteringKeyColumns: [{
-       column: {
-        columnName: 'creation_date',
-        columnType: 'timestamp',
-       },
-       orderBy: 'DESC'
-    }],
+      ],
+      clusteringKeyColumns: [{
+        column: {
+          columnName: 'creation_date',
+          columnType: 'timestamp',
+        },
+        orderBy: 'DESC'
+      }],
       defaultTimeToLive: 0,
       regularColumns: tableColumns.heatids
     }).addDependsOn(keyspace)
 
+    const storechannelQueue = new sqs.Queue(this, 'storechannel', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+      queueName: 'cdkstorechannel',
+      retentionPeriod: cdk.Duration.seconds(1800),
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const mainchannelQueue = new sqs.Queue(this, 'mainchannel', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+      queueName: 'cdkmainchannel',
+      retentionPeriod: cdk.Duration.seconds(1800),
+    });
+
+    const iotActionsRole = new iam.Role(this, 'iot-actions-role', {
+      assumedBy: new iam.ServicePrincipal('iot.amazonaws.com')
+    });
+
+    mainchannelQueue.grantSendMessages(iotActionsRole)
+    storechannelQueue.grantSendMessages(iotActionsRole)
+
+    const sqsMainActionProperty: iot.CfnTopicRule.SqsActionProperty = {
+      queueUrl: mainchannelQueue.queueUrl,
+      roleArn: iotActionsRole.roleArn
+    };
+
+    const mainActionProperty: iot.CfnTopicRule.ActionProperty = {
+      sqs: sqsMainActionProperty,
+    }
+
+    const sqsMainPayloadProperty: iot.CfnTopicRule.TopicRulePayloadProperty = {
+      actions: [mainActionProperty],
+      sql: "SELECT * FROM 'mainchannel'",
+    };
+
+    new iot.CfnTopicRule(this, 'IotTopicMain', {
+      ruleName: "cdkmainchannel",
+      topicRulePayload: sqsMainPayloadProperty
+    });
+
+
+    const sqsStoreActionProperty: iot.CfnTopicRule.SqsActionProperty = {
+      queueUrl: storechannelQueue.queueUrl,
+      roleArn: iotActionsRole.roleArn
+    };
+
+    const storeActionProperty: iot.CfnTopicRule.ActionProperty = {
+      sqs: sqsStoreActionProperty,
+    }
+
+    const sqsStorePayloadProperty: iot.CfnTopicRule.TopicRulePayloadProperty = {
+      actions: [storeActionProperty],
+      sql: "SELECT * FROM 'storechannel'",
+    };
+
+    new iot.CfnTopicRule(this, 'IotTopicStore', {
+      ruleName: "cdkstorechannel",
+      topicRulePayload: sqsStorePayloadProperty
+    });
+
+    new cdk.CfnOutput(this, 'mainchannelQueue', {
+      value: mainchannelQueue.queueUrl,
+    });
+
+    new cdk.CfnOutput(this, 'storechannelQueue', {
+      value: storechannelQueue.queueUrl,
+    });
+
   }
 }
