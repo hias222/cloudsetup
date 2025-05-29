b4266cdef3920674b50f7a97eb6633cea58abae8
matthias <matthiasfuchs@me.com>
2025-05-29 10:18:28 +0200

    add easywk

--- keyspaces/bin/keyspaces.ts
+++ keyspaces/bin/keyspaces.ts
@@ -3,8 +3,10 @@ import * as cdk from '@aws-cdk/core';
 import { Ec2serverStack } from '../lib/ec2server-stack';
 import { KeyspacesStack } from '../lib/keyspaces-stack';
 import { WebSocketOnly } from '../lib/websocketonly-stack';
+import { EasyWk } from '../lib/easywk';
 
 const app = new cdk.App();
 new KeyspacesStack(app, 'KeyspacesStack');
 new Ec2serverStack(app, "Ec2Stack");
 new WebSocketOnly(app, "WebSocketOnly");
+new EasyWk(app, "EasyWk");
